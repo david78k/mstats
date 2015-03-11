afa137bf0779fb9c40973a075aa550ea5fea4126
david78k <david78k@gmail.com>
2015-03-10 21:44:15 -0400

    prelim

--- stats/rowMins.R
+++ stats/rowMins.R
@@ -1,1 +1,14 @@
+library('matrixStats')
+
 x <- matrix(1:50, nrow = 5, ncol = 10)
+print (x)
+
+#y <- rowMins_R(x)
+
+y <- matrixStats::colVars(x)
+print (y)
+
+#y <- apply(x, MARGIN = 2, FUN = vars)
+
+y <- colVars(x, rows = -1:20, cols = 101:400)
+print (y)
